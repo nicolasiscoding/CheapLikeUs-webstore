b6bbc45b94713810f242ba8cb5e67e19d27a7ff0
nicolasiscoding <nicolascoding@gmail.com>
2015-11-17 21:44:56 -0500

    first decent rough commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// set variables for environment
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//start the mysql interface
+var mysql = require('mysql');
+var mysql      = require('mysql');
+var connectionPool = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'toor',
+  database : 'schema'
+});
+ 
+// connection.connect();
+ 
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+ 
+//   console.log('The solution is: ', rows[0].solution);
+// });
+ 
+// connection.end();
+
+// Set server port
+app.listen(1337);
+console.log('server is running at 127.0.0.1:1337');
+
+// views as directory for all template files
+app.set('views', path.join(__dirname, 'views'));
+       
+// instruct express to server up static assets
+app.use(express.static('public'));
+
+// set routes
+app.get('/', function(req, res) {
+  	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/:itemname', function(req,res)
+{
+	console.log('here!');
+	connectionPool.getConnection(function(err, connection)
+	{
+		if(err)
+		{
+			console.log('connection error: \n\n\n');
+			console.log(err);
+			res.statusCode = 503;
+			res.send({
+				result: 'error',
+				err: 	err.code
+			});
+		}
+		else
+		{
+			var query = 'SELECT * FROM Product WHERE name LIKE \'%' + req.params.itemname +'%\' ORDER BY ProductID asc';
+			console.log(query);
+			connection.query(query, req.params.id, function(err, rows, fields)
+			{
+				if(err)
+				{
+					console.log(err);
+					res.statusCode = 500;
+					res.send({
+						result: 'error',
+						err: 	err.code
+					});
+				}
+				else
+				{
+					res.send({
+						result: 'success',
+						err: 	'',
+						fields: fields,
+						json: 	rows,
+						length: rows.length
+					});
+				}
+			});
+			connection.release();
+		}
+	});
+	// connection.destroy();
+});
